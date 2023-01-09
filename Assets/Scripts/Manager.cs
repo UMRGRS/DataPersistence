bff83f2b39a0b95cffd7cc47c018314cfb6bc3a3
UMRGRS <qxrvynilp@gmail.com>
2023-01-08 20:05:07 -0700

    Setting up main menu

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    public static Manager instance;
+
+    public string playersName;
+    public int points;
+
+
+}
